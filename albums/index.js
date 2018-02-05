4ac3be44a323ef1ce79c88486f2dd8f44680b683
Steve Zimmers <steve@soccer-1.com>
2018-02-05 14:57:50 -0500

    create a test component

--- albums/index.js
+++ albums/index.js
@@ -1,4 +1,21 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// import lib to help create a component
+import React from 'react';
+import {AppRegistry, Text} from 'react-native';
 
+// create a component
+// don't need braces and return if return is only thing
+/*
+const App = () => {
+	return (
+		<Text>i like soup. soup is good food.</Text>
+	);
+};
+*/
+
+const App = () => (
+	<Text>i like soup. soup is good food.</Text>
+);
+
+
+// render it
 AppRegistry.registerComponent('albums', () => App);
