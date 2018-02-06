9d22339c3ca811ddc8b131e5dc5fd64d2af36826
Steve Zimmers <steve@soccer-1.com>
2018-02-06 16:28:20 -0500

    start making card components

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
-import {Text, ScrollView} from 'react-native';
+import {Text, View} from 'react-native';
 
 const AlbumDetail = (props) => {
 	//const {textStyle, viewStyle} = styles;
 
 	return (
-		<Text>{props.album.title}</Text>
+		<View>
+			<Text>{props.album.title}</Text>
+		</View>
 		/*
 		<View style={viewStyle}>
 			<Text style={textStyle}>{props.title}</Text>
