68b5f43f4d660369c6059f7aa6e6145cd8254324
Steve Zimmers <steve@soccer-1.com>
2018-02-06 11:42:11 -0500

    more header styling

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 
 const Header = () => {
-	const {textStyle} = styles;
+	const {textStyle, viewStyle} = styles;
 
-	return <Text style={textStyle}>Albums</Text>
+	return (
+		<View style={viewStyle}>
+			<Text style={textStyle}>Albums</Text>
+		</View>
+	);
 };
 
+// justifyContent: align vertically
+// alignItems: align horizontally
 const styles = {
 	textStyle: {
 		fontSize: 20
+	},
+	viewStyle: {
+		backgroundColor: '#f8f8f8',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		paddingTop: 15,
+		shadowColor: '#000',
+		shadowOffset: {width: 0, height: 2},
+		shadowOpacity: 0.4,
+		elevation: 2,
+		position: 'relative'
 	}
 };
 
