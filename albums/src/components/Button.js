04ffbd6f219fe315ff1cde21984343df6ec91592
Steve Zimmers <steve@soccer-1.com>
2018-02-07 14:18:57 -0500

    pass in button label

--- albums/src/components/Button.js
+++ albums/src/components/Button.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Text, TouchableOpacity} from 'react-native';
 
-const Button = ({onPress}) => {
+const Button = ({children, onPress}) => {
 	const {buttonStyle, textStyle} = styles;
 
 	return (
 		<TouchableOpacity style={buttonStyle} onPress={onPress}>
 			<Text style={textStyle}>
-				Buy
+				{children}
 			</Text>
 		</TouchableOpacity>
 	);
