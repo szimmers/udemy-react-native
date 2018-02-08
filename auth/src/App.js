a50a1940501793e842a53e2b0a5aacd78cd78f33
Steve Zimmers <steve@soccer-1.com>
2018-02-08 11:58:51 -0500

    add firebase for email auth

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,22 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import firebase from 'firebase';
 import {Header} from './components/common';
 
 class App extends Component {
+	componentWillMount() {
+		const config = {
+			apiKey: "AIzaSyCNtfEgJXxKwjG0RJrthb8p6qG4NvrwdNo",
+			authDomain: "udemy-react-native-auth-7bf47.firebaseapp.com",
+			databaseURL: "https://udemy-react-native-auth-7bf47.firebaseio.com",
+			projectId: "udemy-react-native-auth-7bf47",
+			storageBucket: "udemy-react-native-auth-7bf47.appspot.com",
+			messagingSenderId: "500220053"
+		};
+
+		firebase.initializeApp(config);
+	}
+
 	render() {
 		return (
 			<View>
