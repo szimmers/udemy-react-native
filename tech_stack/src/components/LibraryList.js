c06a60c2d3d7a1925163245de6bc0a9c4872f796
Steve Zimmers <steve@soccer-1.com>
2018-02-12 11:57:16 -0500

    add library JSON and start reducing it into app

---
+++ tech_stack/src/components/LibraryList.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import {connect} from 'react-redux';
+
+class LibraryList extends Component {
+	render() {
+		console.log(this.props);
+		return <View/>;
+	}
+}
+
+const mapStateToProps = state => {
+	//console.log(state);
+	return {libraries: state.libraries};
+};
+
+export default connect(mapStateToProps)(LibraryList);
