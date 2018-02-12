deb375f1c00c2dc7687a0fc867d752cb597abf44
Steve Zimmers <steve@soccer-1.com>
2018-02-12 13:44:34 -0500

    - more styling in library list item - on main view, ensure we can use full screen (i.e. flex: 1)

--- tech_stack/src/components/LibraryListItem.js
+++ tech_stack/src/components/LibraryListItem.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
 import {Text} from 'react-native';
+import {CardSection} from './common';
 
 class LibraryListItem extends Component {
 	render() {
-		return <Text>{this.props.library.title}</Text>
+		const {titleStyle} = styles;
+
+		return (
+			<CardSection>
+				<Text style={titleStyle}>
+					{this.props.library.title}
+				</Text>
+			</CardSection>
+		);
 	}
 }
+
+const styles = {
+	titleStyle: {
+		fontSize: 18,
+		paddingLeft: 15
+	}
+};
 
 export default LibraryListItem;
