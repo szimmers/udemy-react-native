71bef1cfab9021f6c852176b8166586ca530cca2
Steve Zimmers <steve@soccer-1.com>
2018-02-12 15:43:16 -0500

    add some simple animation

--- tech_stack/src/components/LibraryListItem.js
+++ tech_stack/src/components/LibraryListItem.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
-import {TouchableWithoutFeedback, Text, View} from 'react-native';
+import {TouchableWithoutFeedback, Text, View, LayoutAnimation} from 'react-native';
 import {connect} from 'react-redux';
 import {CardSection} from './common';
 import * as actions from '../actions';
 
 class LibraryListItem extends Component {
+	componentWillUpdate() {
+		LayoutAnimation.spring();
+	}
+
 	renderDescription(expanded, description) {
 		if (expanded) {
 			const {descriptionStyles} = styles;
