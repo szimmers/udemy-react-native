5f24aed5c47743164ec81c122901a7045af74191
Steve Zimmers <steve@soccer-1.com>
2018-02-13 15:20:25 -0500

    add stuff for password

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Button, Card, CardSection, LabeledTextInput} from './common';
-import {emailChanged} from '../actions';
+import {emailChanged, passwordChanged} from '../actions';
 
 class LoginForm extends Component {
 	onEmailChange(text) {
 		this.props.emailChanged(text);
+	}
+
+	onPasswordChange(text) {
+		this.props.passwordChanged(text);
 	}
 
 	render() {
@@ -24,6 +28,8 @@ class LoginForm extends Component {
 					<LabeledTextInput
 						secureTextEntry
 						label='Password'
+						value={this.props.password}
+						onChangeText={this.onPasswordChange.bind(this)}
 					/>
 				</CardSection>
 
@@ -39,8 +45,9 @@ class LoginForm extends Component {
 
 const mapStateToProps = (state) => {
 	return {
-		email: state.auth.email
+		email: state.auth.email,
+		password: state.auth.password
 	}
 };
 
-export default connect(mapStateToProps, {emailChanged})(LoginForm);
+export default connect(mapStateToProps, {emailChanged, passwordChanged})(LoginForm);
