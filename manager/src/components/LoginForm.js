f36b127fa42481f7f1c83c727c464e9a35ce2b32
Steve Zimmers <steve@soccer-1.com>
2018-02-13 15:13:48 -0500

    create action/reducer for email changing

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,7 +1,13 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {Button, Card, CardSection, LabeledTextInput} from './common';
+import {emailChanged} from '../actions';
 
 class LoginForm extends Component {
+	onEmailChange(text) {
+		this.props.emailChanged(text);
+	}
+
 	render() {
 		return (
 			<Card>
@@ -9,6 +15,7 @@ class LoginForm extends Component {
 					<LabeledTextInput
 						label='e-mail'
 						placeholder='username@domain.tld'
+						onChangeText={this.onEmailChange.bind(this)}
 					/>
 				</CardSection>
 
@@ -29,4 +36,4 @@ class LoginForm extends Component {
 	}
 }
 
-export default LoginForm;
+export default connect(null, {emailChanged})(LoginForm);
---
+++ manager/src/reducers/AuthReducer.js
@@ -0,0 +1,15 @@
+import {EMAIL_CHANGED} from '../actions/types';
+
+const INITIAL_STATE = {
+	email: '',
+	password: ''
+};
+
+export default (state = INITIAL_STATE, action) => {
+	switch (action.type) {
+		case EMAIL_CHANGED:
+			return {...state, email: action.payload};
+		default:
+			return state;
+	}
+};
