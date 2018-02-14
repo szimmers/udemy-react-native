5f9a33ce2428f5b171f33bcaceec5275e233773b
Steve Zimmers <steve@soccer-1.com>
2018-02-14 12:33:51 -0500

    - add action creator for logging in using thunk - wire up login button

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Button, Card, CardSection, LabeledTextInput} from './common';
-import {emailChanged, passwordChanged} from '../actions';
+import {emailChanged, passwordChanged, loginUser} from '../actions';
 
 class LoginForm extends Component {
 	onEmailChange(text) {
@@ -10,6 +10,11 @@ class LoginForm extends Component {
 
 	onPasswordChange(text) {
 		this.props.passwordChanged(text);
+	}
+
+	onLogin() {
+		const {email, password} = this.props;
+		this.props.loginUser({email, password});
 	}
 
 	render() {
@@ -34,7 +39,7 @@ class LoginForm extends Component {
 				</CardSection>
 
 				<CardSection>
-					<Button>
+					<Button onPress={this.onLogin.bind(this)}>
 						Log In
 					</Button>
 				</CardSection>
@@ -52,4 +57,4 @@ const mapStateToProps = (state) => {
 	}
 };
 
-export default connect(mapStateToProps, {emailChanged, passwordChanged})(LoginForm);
+export default connect(mapStateToProps, {emailChanged, passwordChanged, loginUser})(LoginForm);
