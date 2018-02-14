e0f0133a6169e69bf2337193fca1030f78096ae8
Steve Zimmers <steve@soccer-1.com>
2018-02-14 13:31:02 -0500

    add reducer for logged in user

--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -1,8 +1,9 @@
-import {EMAIL_CHANGED, PASSWORD_CHANGED} from '../actions/types';
+import {EMAIL_CHANGED, LOGIN_USER_SUCCESS, PASSWORD_CHANGED} from '../actions/types';
 
 const INITIAL_STATE = {
 	email: '',
-	password: ''
+	password: '',
+	user: null
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -11,6 +12,8 @@ export default (state = INITIAL_STATE, action) => {
 			return {...state, email: action.payload};
 		case PASSWORD_CHANGED:
 			return {...state, password: action.payload};
+		case LOGIN_USER_SUCCESS:
+			return {...state, user: action.payload};
 		default:
 			return state;
 	}
