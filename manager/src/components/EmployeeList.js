06b86d834a37e7a732fcf1966c7ef50842953022
Steve Zimmers <steve@soccer-1.com>
2018-02-15 11:36:44 -0500

    start employee list component

---
+++ manager/src/components/EmployeeList.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class EmployeeList extends Component {
+	render() {
+		return (
+			<View>
+				<Text>tim</Text>
+				<Text>bob</Text>
+				<Text>larry</Text>
+				<Text>doug</Text>
+			</View>
+		);
+	}
+}
+
+export default EmployeeList;
