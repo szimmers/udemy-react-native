5184b5bccea43b6b4a312eb40594a6843bfb0bf3
Steve Zimmers <steve@soccer-1.com>
2018-02-15 13:26:55 -0500

    more employee form reducer

--- manager/src/reducers/EmployeeFormReducer.js
+++ manager/src/reducers/EmployeeFormReducer.js
@@ -1,11 +1,17 @@
 import {EMPLOYEE_UPDATE} from '../actions/types';
 
 const INITIAL_STATE = {
-
+	name: '',
+	phone: '',
+	shift: ''
 };
 
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
+		case EMPLOYEE_UPDATE:
+			// e.g. action.payload === {prop: 'phone', value: '312-276-7450'}
+			return {...state, [action.payload.prop]: action.payload.value};
+
 		default:
 			return state;
 	}
