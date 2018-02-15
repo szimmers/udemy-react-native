84cf560b04842b8444fceb0570f1425cb3529891
Steve Zimmers <steve@soccer-1.com>
2018-02-15 11:41:30 -0500

    navigate after login

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import {Actions} from 'react-native-router-flux';
 import {EMAIL_CHANGED, LOGIN_USER, LOGIN_USER_FAIL, LOGIN_USER_SUCCESS, PASSWORD_CHANGED} from './types';
 
 export const emailChanged = (text) => {
@@ -20,6 +21,8 @@ const userLoginSuccess = (dispatch, user) => {
 		type: LOGIN_USER_SUCCESS,
 		payload: user
 	});
+
+	Actions.employeeList();
 };
 
 const userLoginFail = (dispatch) => {
