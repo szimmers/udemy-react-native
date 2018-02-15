878982e05edd258cd2b02e82368baa3c4736d8e3
Steve Zimmers <steve@soccer-1.com>
2018-02-15 15:39:54 -0500

    re-org reducers

---
+++ manager/src/actions/AuthActions.js
@@ -0,0 +1,44 @@
+import firebase from 'firebase';
+import {Actions} from 'react-native-router-flux';
+import {EMAIL_CHANGED, LOGIN_USER, LOGIN_USER_FAIL, LOGIN_USER_SUCCESS, PASSWORD_CHANGED} from './types';
+
+export const emailChanged = (text) => {
+	return {
+		type: EMAIL_CHANGED,
+		payload: text
+	}
+};
+
+export const passwordChanged = (text) => {
+	return {
+		type: PASSWORD_CHANGED,
+		payload: text
+	}
+};
+
+const userLoginSuccess = (dispatch, user) => {
+	dispatch({
+		type: LOGIN_USER_SUCCESS,
+		payload: user
+	});
+
+	Actions.main();
+};
+
+const userLoginFail = (dispatch) => {
+	dispatch({type: LOGIN_USER_FAIL});
+};
+
+export const loginUser = ({email, password}) => {
+	return (dispatch) => {
+		dispatch({type: LOGIN_USER});
+
+		firebase.auth().signInWithEmailAndPassword(email, password)
+			.then(user => userLoginSuccess(dispatch, user))
+			.catch(() => {
+				firebase.auth().createUserWithEmailAndPassword(email, password)
+					.then(user => userLoginSuccess(dispatch, user))
+					.catch(() => userLoginFail(dispatch));
+			});
+	};
+};
