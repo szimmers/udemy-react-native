90ebf357609daa43dc777a5393e76936e238dfcc
Steve Zimmers <steve@soccer-1.com>
2018-02-15 13:02:50 -0500

    build out more of employee detail form

--- manager/src/components/EmployeeDetail.js
+++ manager/src/components/EmployeeDetail.js
@@ -1,12 +1,34 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import {Button, Card, CardSection, LabeledTextInput} from './common';
 
 class EmployeeDetail extends Component {
 	render() {
 		return (
-			<View>
-				<Text>new</Text>
-			</View>
+			<Card>
+				<CardSection>
+					<LabeledTextInput
+						label="Name"
+						placeholder="Tim"
+					/>
+				</CardSection>
+
+				<CardSection>
+					<LabeledTextInput
+						label="Phone"
+						placeholder="555-555-5555"
+					/>
+				</CardSection>
+
+				<CardSection>
+				</CardSection>
+
+				<CardSection>
+					<Button>
+						Create
+					</Button>
+				</CardSection>
+			</Card>
 		);
 	}
 }
