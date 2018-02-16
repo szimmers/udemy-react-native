83ae17f114d05e3f446a57aac8cbb1249a26a1dd
Steve Zimmers <steve@soccer-1.com>
2018-02-16 11:57:39 -0500

    save new employee to firebase

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -1,14 +1,28 @@
+import firebase from 'firebase';
 import {EMPLOYEE_CREATE, EMPLOYEE_UPDATE} from './types';
 
+/**
+ * updates when user types in field or changes picker
+ * @param prop
+ * @param value
+ * @returns {{type: string, payload: {prop: *, value: *}}}
+ */
 export const employeeUpdate = ({prop, value}) => {
-
 	return {
 		type: EMPLOYEE_UPDATE,
 		payload: {prop, value}
 	}
 };
 
+/**
+ * user saves employee form
+ * @param name
+ * @param phone
+ * @param shift
+ */
 export const employeeCreate = ({name, phone, shift}) => {
-	console.log(name, phone, shift);
 	//type: EMPLOYEE_CREATE,
+	const {currentUser} = firebase.auth();
+	firebase.database().ref(`users/${currentUser.uid}/employees`)
+		.push({name, phone, shift});
 };
